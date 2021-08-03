cb1a916f40b991f0f66acb803c5790c565df8ae0
timor.rust <75886852+timorust@users.noreply.github.com>
2021-08-03 09:17:32 +0300

    Logger consol & file

--- express.js
+++ express.js
@@ -2,9 +2,11 @@ const express = require('express')
 const path = require('path')
 const acc_repo = require('./account_repo')
 
-
 const port = 8080;
 const app = express();
+
+const { createLogger, format, transports } = require('winston');
+const { combine, timestamp, label, prettyPrint } = format;
 
 app.use(express.static(path.join(__dirname, '/')))
 
@@ -13,17 +15,59 @@ app.use(express.urlencoded({
   extended: true
 }))
 
-app.get('/accounts', async(req, res) => {
-  const account = await acc_repo.getAllAccount(); 
-  res.status(200).json({account})
+
+
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'postgres',
+  password: 'admin',
+  port: 5432,
+})
+
+const logger = createLogger({
+  format: combine(
+    label({ label: 'main logger!' }),
+    timestamp(),
+    prettyPrint()
+  ),
+  transports: [new transports.Console(),
+    new transports.File({
+        filename: 'example.log',
+        level: 'info'})
+    ]
+})
+
+
+logger.log({
+  level: 'info',
+  message: '*********** starting app ***********'
+});
+
+
+app.get('/pg_accounts', async(req, res) => {
+  pool.query('SELECT * FROM account', (error, results) => {
+          if (error) {
+           logger.log({
+               level: 'error',
+               message: `Error: ${error}`
+           });
+           res.status(500).json({'error': error.toString()})
+        }
+        else 
+        res.status(200).json(results.rows)
+    })
 });
 
 app.get('/accounts/:acc_id', async(req, res) => {
+    // log debug --
+    // starting function: app.get('/accounts/:acc_id',...)
+    // parameters: req, res, req.params.acc_id 
   const id = req.params.acc_id;
   const account = await acc_repo.getAccountById(id); 
   res.status(200).json({account})
 });
-
 
 
 app.post('/accounts', async (req, res) => {
@@ -38,6 +82,16 @@ app.post('/accounts', async (req, res) => {
         })
     }
     catch(e) {
+
+        logger.log({
+               level: 'error',
+               message: `app.post('/accounts'). Error: ${e}`
+           });
+           logger.log({
+               level: 'error',
+               message: `sql dump: sql insert for object ${JSON.stringify(acc)}`
+           });
+           
         res.status(400).send({
             status: 'fail',
             message: e.message
@@ -58,31 +112,45 @@ app.put('/accounts/:acc_id', async(req, res) => {
         })
     }
     catch(e) {
-        res.status(400).send({
-            status: 'fail',
-            message: e.message
-        })
-    }
+        // failed to write to db
+        // write all the db info for the operation
+    const logger = createLogger({
+    format: combine(
+        label({ label: 'main logger!' }),
+        timestamp(),
+        prettyPrint()
+     ),
+    transports: [new transports.Console(),
+    new transports.File({
+        filename: 'example.log',
+        level: 'info'})]
+    })
+    res.status(400).send({
+        status: 'fail',
+        message: e.message
+    })
+   }
 });
 
-app.delete('/accounts/:acc_id', async(req, res) => {
-    try
-    {
-        const acc_id = req.params.acc_id
-        const result = await acc_repo.deleteAccount(acc_id)
-        res.status(200).json({
-            res: 'success',
-            url: `localhost:8080/accounts/${acc_id}`,
-            result
-        })
-    }
-    catch(e) {
-        res.status(400).send({
-            status: 'fail',
-            message: e.message
-        })
-    }
-});
+
+// app.delete('/accounts/:acc_id', async(req, res) => {
+//     try
+//     {
+//         const acc_id = req.params.acc_id
+//         const result = await acc_repo.deleteAccount(acc_id)
+//         res.status(200).json({
+//             res: 'success',
+//             url: `localhost:8080/accounts/${acc_id}`,
+//             result
+//         })
+//     }
+//     catch(e) {
+//         res.status(400).send({
+//             status: 'fail',
+//             message: e.message
+//         })
+//     }
+// });
 
 
 app.listen(port, () => console.log(`Listening to port ${port}`))
